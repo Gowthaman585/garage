1f631a07079691c249c65107e37c3724abeee050
Gowthaman M <gowthamanm585@gmail.com>
2026-01-09 22:19:02 +0530

    Updated for next module 50%

--- python-playground/regex-practise.py
+++ python-playground/regex-practise.py
@@ -1,6 +1,5 @@
 import re
 
-
 # Opening the file in read mode to analyze
 with open("copy.txt",'r') as file:
 	lines = file.readlines()
@@ -11,6 +10,9 @@ ssh_pass = 0
 # incrementing variable for failed ssh-login attempts
 ssh_fail = 0
 
+# incrementing variable for no of successful disconnections
+ssh_disc = 0
+
 # snippet to count the ssh-log lines particularly
 for i in lines:
 	if re.search("sshd",i):
@@ -18,7 +20,10 @@ for i in lines:
 			ssh_pass = ssh_pass + 1
 		if re.search("Failed password",i):
 			ssh_fail = ssh_fail + 1
+		if re.search("Disconnected from",i):
+			ssh_disc = ssh_disc + 1
 
 # displaying  the detials for ssh-login event details
 print(f"Total no of  successful ssh-login attempts : {ssh_pass}")
 print(f"Total no of failed ssh-login attempts : {ssh_fail}")
+print(f"Total no of successful ssh-disconnection's : {ssh_disc}")
